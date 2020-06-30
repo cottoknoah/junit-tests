b76bbb535cba053264cc8d047531fe049bb47bc4
Knoah Cotto <cotto.knoah@gmail.com>
2020-06-30 15:44:28 -0500

    work

---
+++ src/test/java/Student2.java
@@ -0,0 +1,52 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class Student {
+    //properties
+    public String name;
+    public long id;
+
+    //constructor
+    public Student() {
+        this.name = name;
+        this.id = id;
+        //initializes the grades property as an empty ArrayList.
+
+    }
+
+    //getter
+    public String getName() {
+        return name;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // returns the student's id
+    public long getId(){
+        return this.id;
+    }
+
+    // returns the student's name
+    public String getName(){
+        return this.name = " ";
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+        return
+    }
+
+
+}
---
+++ src/test/java/Student2Test.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+
+    @Before
+    public void setUp() {
+        Student blank = new Student();
+        blank.name = "Knoah";
+        blank.id = 1234567890;
+    }
+
+    @Test
+    public void getId() {
+
+    }
+
+}
